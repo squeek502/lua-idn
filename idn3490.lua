1fb31e742e5463132f90363a40da171bbd9593d4
Ryan Liptak <squeek502@hotmail.com>
2018-07-17 17:35:27 -0700

    Remove hard dependency on luabitop
    Will use Lua 5.3 bitwise operators or bit32 if its available

--- idn3490.lua
+++ idn3490.lua
@@ -1,4 +1,21 @@
-local bit = require'bit'
+local bit = (function()
+	if _VERSION == "Lua 5.3" then
+		return assert(load([[return {
+			band = function(a, b) return a & b end,
+			rshift = function(a, b) return a >> b end,
+			lshift = function(a, b) return a << b end,
+		}]]))()
+	end
+
+	local hasBit32, bit32 = pcall(require, "bit32")
+	if hasBit32 then return bit32 end
+
+	local hasBit, bitop = pcall(require, "bit")
+	if hasBit then return bitop end
+end)()
+if not bit then
+	error("no bit library available (looked for Lua 5.3 operators, 'bit32', 'bit')")
+end
 
 local base = 36
 local tmin = 1
